72b13111a03d6c2bc5cb1f8f308d484ef5ca0768
Kyungmin Lee <banjjagi@naver.com>
2016-05-14 16:56:12 +0900

    bug fixed

--- LottoGenerator.java
+++ LottoGenerator.java
@@ -1,4 +1,4 @@
-ackage project;
+package project;
 import java.util.Random;
 import java.util.Arrays;
 public class LottoGenerator {
@@ -7,7 +7,7 @@ public class LottoGenerator {
                int[] result = new int[6];
 }
 for(int i = 0; i < 6; i++) {
-       result[i] = random.nextInt(45);
+       result[i] = random.nextInt(45)+1;
 }
 return result;
        public static void main(String[] args) {
